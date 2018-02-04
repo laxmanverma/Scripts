bec49b331a0603788cc4eb697a1fba628269540d
Laxman Verma <laxmanbravolook@gmail.com>
2018-02-04 11:49:00 +0530

    mensXp and maxim women articles

--- mensXp/women.py
+++ mensXp/women.py
@@ -20,7 +20,7 @@ htmlCodes = (
 config = {
   "apiKey": "****",
   "authDomain": "****.firebaseapp.com",
-  "databaseURL": "https://*****.firebaseio.com",
+  "databaseURL": "https://****.firebaseio.com",
   "storageBucket": "****.appspot.com"
 }
 
